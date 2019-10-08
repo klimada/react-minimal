6965bd1e8b2a489d23931dcd74baffeeb321e503
klimada <56273947+klimada@users.noreply.github.com>
2019-10-08 09:52:55 +0200

    init4

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './MyApp'
 
-render(<App />, document.getElementById('reactinjectedhere'))
+render(<App />, document.getElementById('root'))
