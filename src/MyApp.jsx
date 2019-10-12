581d3dbb2fa9bb213b7d341f736851320d2bb89c
klimada <56273947+klimada@users.noreply.github.com>
2019-10-12 16:00:30 +0200

    Production bundle analysis

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const App = () => (
-  <h1>Minimal React klimada</h1>  
-)
+  <div>
+  <h1>Minimal React klimada</h1>
+  <p1>Bundle size: 40700 bytes, Load time of the bundle: 439 ms, Last commit SHA1: 4DB7A670336A22881FB00AA5648787E20CDF7A19</p1> 
+  </div>
+  )
 
 export default App
