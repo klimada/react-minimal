d25feb5243c43503f6cfcd068372522b5bc34abb
klimada <56273947+klimada@users.noreply.github.com>
2019-10-12 18:28:44 +0200

    Lab1-poprawilam na wyswietlanie w consolelog wszystkiego

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -7,15 +7,7 @@ const arrayno2 = [2, 5, 8, 10];
 const MyApp = () => (
   <div className="MyApp"> 
     <Apptitle/>
-    <p1>Bundle size: 40700 bytes, Load time of the bundle: 439 ms, Last commit SHA1: 4DB7A670336A22881FB00AA5648787E20CDF7A19</p1> 
-    <h2>Exercise 2</h2>
-    <p2>{ generateArray(5).map(x => <li>{x}</li>) }</p2>
-    <h2>Exercise 3</h2>
-    <p2>{ generateRandomArray(5).map(x => <li>{x}</li>) }</p2>
-    <h2>Exercise 4</h2>
-    <p2>{ showGreaterThan15(arrayno1).map(x => <li>{x}</li>) }</p2>
-    <h2>Exercise 5</h2>
-    <p3>{ squareArray(arrayno2).map(x => <li>{x}</li>) }</p3>
+    <p>Bundle size: 40700 bytes, Load time of the bundle: 439 ms, Last commit SHA1: 4DB7A670336A22881FB00AA5648787E20CDF7A19</p> 
   </div>
   )
 
@@ -24,4 +16,14 @@ const generateRandomArray = (n) => Array.from(new Array(n), (val) => Math.floor(
 const showGreaterThan15 = (x) => x.filter((a) => a > 15);
 const squareArray = (x) => x.map((a) => Math.sqrt(a));
 
+{console.log("exercise 2")}
+{console.log(generateArray(5))}
+{console.log("exercise 3")}
+{console.log(generateRandomArray(5))}
+{console.log("exercise 4")}
+{console.log(showGreaterThan15(arrayno1))}
+{console.log("exercise 5")}
+{console.log(squareArray(arrayno2))}
+
 export default MyApp
+
