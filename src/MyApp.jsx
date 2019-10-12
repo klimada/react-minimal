0472750a6934692fee7526e40ca5d397f9c5e1d6
klimada <56273947+klimada@users.noreply.github.com>
2019-10-12 16:11:34 +0200

    Production bundle analysis

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Apptitle from './AppTitle.js'
 
 const App = () => (
   <div>
-  <h1>Minimal React klimada</h1>
-  <p1>Bundle size: 40700 bytes, Load time of the bundle: 439 ms, Last commit SHA1: 4DB7A670336A22881FB00AA5648787E20CDF7A19</p1> 
+    <Apptitle></Apptitle>
+    <p1>Bundle size: 40700 bytes, Load time of the bundle: 439 ms, Last commit SHA1: 4DB7A670336A22881FB00AA5648787E20CDF7A19</p1> 
   </div>
   )
 
