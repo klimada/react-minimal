83f3de9d0182cb9e26a43a89b94a7bf65962c375
klimada <56273947+klimada@users.noreply.github.com>
2019-10-12 16:18:55 +0200

    Lab1ex1

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -3,7 +3,8 @@ import Apptitle from './AppTitle.js'
 
 const MyApp = () => (
   <div className="MyApp"> 
-    
+    <Apptitle/>
+    <p1>Bundle size: 40700 bytes, Load time of the bundle: 439 ms, Last commit SHA1: 4DB7A670336A22881FB00AA5648787E20CDF7A19</p1> 
   </div>
   )
 
