e9e324964938de480edfeb5ee7b790f1f2bf46cb
klimada <56273947+klimada@users.noreply.github.com>
2019-10-12 19:35:51 +0200

    Lab1ex7

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -4,12 +4,42 @@ import Apptitle from './AppTitle.js'
 const arrayno1 = [2, 56, 23, 88, 17, 4];
 const arrayno2 = [2, 5, 8, 10];
 
-const MyApp = () => (
-  <div className="MyApp"> 
-    <Apptitle/>
-    <p>Bundle size: 40700 bytes, Load time of the bundle: 439 ms, Last commit SHA1: 4DB7A670336A22881FB00AA5648787E20CDF7A19</p> 
-  </div>
-  )
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { myData: (data.map(x => x.students)).flat()};
+    this.All = this.All.bind(this);
+    this.Sort = this.Sort.bind(this);
+    this.Old = this.Old.bind(this);
+  }
+  
+  All() {
+    this.setState({ myData: (data.map(x => x.students)).flat() });
+  }
+  
+  Sort() {
+    this.setState({ myData: (data.map(x => x.students)).flat().sort((a, b) => a.name.localeCompare(b.name))});
+  }
+  
+  Old() {
+    this.setState({ myData: (data.filter(x => x.active).map(x => x.students)).flat().filter(a => a.age > 20) });
+  }
+  
+  render() {
+    const { myData } = this.state;  
+    return (
+      <div className="MyApp"> 
+       <Apptitle/>
+      <p>Bundle size: 40700 bytes, Load time of the bundle: 439 ms, Last commit SHA1: 4DB7A670336A22881FB00AA5648787E20CDF7A19</p>    
+      <button id="buttonAll" onClick={this.All}>All students</button>
+      <button id="buttonSort" onClick={this.Sort}>Sort students</button>
+      <button id="buttonOld" onClick={this.Old}>Old students</button>
+
+      <ul>{ myData.map(x => <li key={x.name}>{x.name}</li>)}</ul>
+      </div>
+    );
+  }
+}
 
 const generateArray = (n) => Array.from(new Array(n),(val,i) => 1 + i);
 const generateRandomArray = (n) => Array.from(new Array(n), (val) => Math.floor(Math.random() * 25 ) + 1);
@@ -24,6 +54,67 @@ const squareArray = (x) => x.map((a) => Math.sqrt(a));
 {console.log(showGreaterThan15(arrayno1))}
 {console.log("exercise 5")}
 {console.log(squareArray(arrayno2))}
+
+var data =
+[
+  {
+    "teacherName": "Jan Nowak",
+    "teacherAge": 36,
+    "active": true,
+    "students": [
+      {
+        "name": "Maciej Janosz",
+        "age": 12
+      },
+      {
+        "name": "Wojciech Kowalski",
+        "age": 15
+      },
+      {
+        "name": "Wioletta Poznańska",
+        "age": 1000000
+      }
+    ]
+  },
+  {
+    "teacherName": "Mariusz Flasinski",
+    "teacherAge": 56,
+    "active": true,
+    "students": [
+      {
+        "name": "Jan Kot",
+        "age": 12
+      },
+      {
+        "name": "Jan Ziobro",
+        "age": 15
+      },
+      {
+        "name": "Adam Malysz",
+        "age": 41
+      }
+    ]
+  },
+  {
+    "teacherName": "Wojciech Kuzak",
+    "teacherAge": 44,
+    "active": false,
+    "students": [
+      {
+        "name": "Janina Wronska",
+        "age": 22
+      },
+      {
+        "name": "John Dover",
+        "age": 7
+      },
+      {
+        "name": "Emil Petterson",
+        "age": 46
+      }
+    ]
+  }
+]
 
 export default MyApp
 
